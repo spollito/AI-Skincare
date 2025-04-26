7c372f3f7c2b1ee3419e9198466ffb3e155771c8
Steven <stevenburuca1@gmail.com>
2025-04-26 19:56:16 -0400

    Added Main View

--- src/main/java/com/example/skinanalysis/Controller/WebController.java
+++ src/main/java/com/example/skinanalysis/Controller/WebController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 
-    @GetMapping("/")
-    public String home() {
-        return "index";
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "dashboard";
     }
 }
