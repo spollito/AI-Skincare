6aba0e40cb633bcb76ddb607c59246b99eebd5d5
Steven <stevenburuca1@gmail.com>
2025-04-26 14:44:54 -0400

    init commit

---
+++ src/main/java/com/example/skinanalysis/SkinAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.example.skinanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkinAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkinAnalysisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/skinanalysis/SkinAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skinanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkinAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
